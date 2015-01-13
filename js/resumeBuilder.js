8cb359dd63f0525239f535d83efe58cbc58edca2
allbad <neil_allgood@hotmail.com>
2015-01-13 19:41:56 +0000

    updated to maps

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 var bio = {
     "name": "Neil Allgood",
-    "role": "Web Developer",
+    "role": "Front-end Web Developer",
     "contacts": {
         "mobile": "07767 374488",
         "email": "neil_allgood@hotmail.com",
@@ -54,14 +54,14 @@ var work = {
         {
             "employer": "Barclays",
             "title": "Head of Collaboration Eng and Dev",
-            "location": "London",
+            "location": "Canary Wharf, London",
             "dates": "1996-present",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet arcu enim, eget accumsan libero lacinia ut. Ut mattis, augue eget bibendum blandit, sapien nibh auctor risus, eu aliquet quam sem in lorem. Vivamus eget leo mi. Nullam lorem tortor, finibus eget fermentum a, pellentesque ac nunc. In id maximus sem. Morbi in fringilla elit. Vivamus posuere magna id turpis sagittis, vel lobortis enim efficitur."
         },
         {
             "employer": "OC&C Strategy Consultants",
             "title": "Head of Graphics and IT",
-            "location": "London",
+            "location": "Kingsway, London",
             "dates": "1993-1996",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In aliquet arcu enim, eget accumsan libero lacinia ut. Ut mattis, augue eget bibendum blandit, sapien nibh auctor risus, eu aliquet quam sem in lorem. Vivamus eget leo mi. Nullam lorem tortor, finibus eget fermentum a, pellentesque ac nunc. In id maximus sem. Morbi in fringilla elit. Vivamus posuere magna id turpis sagittis, vel lobortis enim efficitur."
         }
@@ -70,16 +70,16 @@ var work = {
 
 function displayWork() {
     for (job in work.jobs){
-    // create new div for work experience
-    $("#workExperience").append(HTMLworkStart);
-    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-    $(".work-entry:last").append(formattedEmployer + formattedTitle);   
-    $(".work-entry:last").append(formattedDates);
-    $(".work-entry:last").append(formattedDescription);
-}
+        // create new div for work experience
+        $("#workExperience").append(HTMLworkStart);
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedEmployer + formattedTitle);   
+        $(".work-entry:last").append(formattedDates);
+        $(".work-entry:last").append(formattedDescription);
+    }
 }
 
 displayWork();
@@ -87,16 +87,14 @@ displayWork();
 var education = {
 	"schools": [
 		{
-			"name": "Howard",
-			"years": "1980-1986",
+			"name": "Howard Grammar",
+			"dates": "1980-1986",
 			"city": "Rainham",
-			"url": "http://www.thehowardschool.co.uk/sitev3/index.php"
 		},
 		{
 			"name": "St Margarets",
-			"years": "1974-1980",
+			"dates": "1974-1980",
 			"city": "Rainham",
-			"url": "http://www.stmargaretsonline.net/"
 		}
 	],	
 	"onlineCourses": [
@@ -115,6 +113,21 @@ var education = {
 	]
 }
 
+education.display = function(){
+    for (school in education.schools){
+        // create new div for schools
+        $("#education").append(HTMLschoolStart);
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        $(".education-entry:last").append(formattedName);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        $(".education-entry:last").append(formattedDates);      
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].city);  
+        $(".education-entry:last").append(formattedLocation);
+    }
+}
+
+education.display();
+
 var projects = {
     "projects": [
         {
@@ -122,8 +135,7 @@ var projects = {
             "dates": 2014,
             "description": "Language website for a friend",
             "images": [
-                "images/neil.jpg",
-                "images/neil.jpg"
+                "images/elrecreo.png"
             ]
         },
         {
@@ -131,12 +143,35 @@ var projects = {
             "dates": 2014,
             "description": "Starter website for me",
             "images": [
-                "images/neil.jpg",
-                "images/neil.jpg"
+                "images/at1.png",
+                "images/at2.png"
             ]
         }
     ]
 }
+
+projects.display = function(){
+    for (project in projects.projects){
+        // create new div for projects
+        $("#projects").append(HTMLprojectStart);
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        $(".project-entry:last").append(formattedTitle);
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);      
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);  
+        $(".project-entry:last").append(formattedDescription);
+        if (projects.projects[project].images.length >0) {
+            for (image in projects.projects[project].images) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+
+projects.display();
+
+
 
 function inName(name){
     name = name.trim().split(" ");
@@ -147,3 +182,6 @@ function inName(name){
 }
 
 $("#main").append(internationalizeButton);
+
+// see a map
+$("#mapDiv").append(googleMap);
